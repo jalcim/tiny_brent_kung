379da25b4fe17eb8ce031e9d92d2c1624b3468fc
jalcim <alcim.dev@gmail.com>
2025-09-14 12:11:10 +0200

    unused bit

--- src/brent-kung.v
+++ src/brent-kung.v
@@ -101,5 +101,6 @@ module tt_um_brent_kung (
 
    // List all unused inputs to prevent warnings
    wire _unused = &{ena, clk, rst_n, 1'b0, ui_in[7:4], uio_in[7:4]};
+   assign ui_out[7:5] = 3'b0;
 
 endmodule
